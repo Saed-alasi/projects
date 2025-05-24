b64da36302fa5b13f2ddbf67cf23c1dec7dc4bf8
saed-alasi <saed.alasi1997@gmail.com>
2025-05-24 12:51:42 +0200

    line 2

---
+++ test.js
@@ -0,0 +1,188 @@
+// var s ="hello"
+// console.log(s)
+// console.log(parseInt("4.55 saed"))
+// console.log(parseFloat("4.55 saed"))
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.isInteger('ss'))
+// console.log(Math.sqrt(9))
+// var z = 2
+// console.log(    0.025 * z + 'شيكل')
+
+// var sa='i love java script'
+// console.log( sa.indexOf('a',9))
+// let ahmed=['saly','monir','ramy','romy']
+// console.log(ahmed[2])
+// ahmed.push('saed')
+// console.log(ahmed)
+// ahmed.unshift('sameer')
+// console.log(ahmed)
+// ahmed.pop()
+// console.log(ahmed)
+// ahmed.splice(2,1,)
+// console.log(ahmed)
+
+//  let num1=10
+//  var num2=6
+//  if(num1>=num2   ){
+//     console.log('hello')
+//  }
+//  else{
+//     console.log('haha')
+//  }
+       // let ahmed=['saly','monir','ramy','romy']
+       // var age  =[ 18   ,  20   ,50    ,  30  ]
+
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    problem 2(https://www.codewars.com/kata/5810085c533d69f4980001cf)
+
+//     function cal (a, b, mark ){
+
+//     if(typeof a =='number' && typeof b=='number'){
+//        if (mark=='+'){
+//           return a+b
+//        }
+//        else if(mark=='-'){
+//               return a-b
+               
+//        }
+//         else if(mark=='/'){
+//               return a/b
+               
+//        }
+//         else if(mark=='*'){
+//               return a*b
+               
+//        }
+//        else{
+//               return  "unknown value"
+//        }
+             
+
+//     }
+//     else{
+
+//        return "unknown value"
+//     }
+  
+//     }
+//    console.log(cal(8,10,'/'))
+  
+
+
+//  problem 3
+
+// function remove (word){
+
+//   return word.replace(/!/g,'')
+
+// }
+// console.log(remove('saed!!!'))
+
+
+
+
+
+//  problem 4
+// function test(number){
+//        if(number%2==0)
+//          return 'even'
+//        else if(number%2==1){
+//               return 'odd'
+//        }
+//        else{
+//            return 'error'  
+//        }
+
+// }
+// console.log(test('ss'))
+
+
+
+
+//  problem 5
+// function ruleDrink(age){
+//        if(age<14  ){
+//         return 'drink toddy'
+
+//        }
+//        else if(age<18){
+//          return 'drink coke'
+//        }
+//          else if(age<21){
+//          return 'drink coke'
+//        }
+//           else if(age>18){
+//          return 'drink whisky'
+//        }
+// }
+// console.log(ruleDrink(17))
+//--------------------------------
+
+//--problem 2  14 
+
+
+// function delet(arr){
+//         const newArr=[]
+
+//         for(i=0   ;i<arr.length   ;i++   ){
+
+
+//             if(i%2==0){
+//               newArr.push(arr[i])
+//             }
+
+
+          
+//         }
+//          return newArr
+// }
+//         console.log(delet(['saed','rany','ahmed','lamed']))
+
+//---------------------------------------
+//--problem 3 14 
+// function test2(a,x){
+//        return a.includes(x)
+
+// }
+//    console.log(test2(['saed','rany','9','10'],'rany'))
+//---------------------------------------------
+//--problem 4 14 
+
+// function family(ages){
+//        const maxValue=Math.max(...ages)
+//        const minValue=Math.min(...ages)
+//       const def = maxValue - minValue
+//        var result=[]
+//        result.push(maxValue,minValue,def)
+
+//        return result
+ 
+// }
+// console.log(family([12, 45, 3, 67, 21]))
+//---------------------------------------------
+//--problem 5 14 
